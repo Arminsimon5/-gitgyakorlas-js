5cdc6507284d0ccc06e0e6c4f3c39012a967b7d7
Simon Ármin <arminsimon5@gmail.com>
2023-02-27 11:25:19 +0100

    1)b feladat

--- javascript.js
+++ javascript.js
@@ -1,9 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("button").addEventListener("click", function(){
+        const input1 = document.getElementById("input1").value;
+        const input2 = document.getElementById("input2").value;
+        const visszaJelzes = document.getElementById("visszaJelzes")
+        visszaJelzes.innerHTML = nagyobb(input1,input2)
+
+}) 
+})
+
 function nagyobb(egesz1,egesz2) {
     if (egesz1>egesz2) {
-        return egesz1
+        return "Az első szám nagyobb: "+egesz1
     }
     else if (egesz1<egesz2) {
-        return egesz2
+        return "Az második szám nagyobb: "+egesz2
     }
     else {
         return "A két szám egyenlő."
