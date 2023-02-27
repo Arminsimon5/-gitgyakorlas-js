5d6d3ce5c4600858db23f3a968a017f4d4ec5b14
Simon Ármin <arminsimon5@gmail.com>
2023-02-27 11:56:36 +0100

    1)d feladat

--- javascript.js
+++ javascript.js
@@ -1,17 +1,19 @@
 const lista = [];
-const max = 0;
+
+
 document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("hozzaAd").addEventListener("click", function(){
         const input1 = document.getElementById("input1").value;
         lista.push(input1)     
     });
     document.getElementById("kiIr").addEventListener("click", function(){
-        document.getElementById("visszaJelzes").innerHTML = "A legnagyobb szám a felsorolásból: "+nagyobb(lista,max)
+        const min = input1 = document.getElementById("input1").value;
+        const max = input1 = document.getElementById("input1").value;
+        document.getElementById("visszaJelzes").innerHTML = "A legnagyobb szám a felsorolásból: "+nagyobb(lista,max)+" A legkisebb szám a felsorolásból: "+kisebb(lista,min)
     });
 });
 
 function nagyobb(lista, max) {
-    max = 0
     for (let index = 0; index < lista.length; index++) {
         const element = lista[index];
         if (element>max) {
@@ -19,5 +21,14 @@ function nagyobb(lista, max) {
         }
     }
     return max
+}
+function kisebb(lista, min) {
+    for (let index = 0; index < lista.length; index++) {
+        const element = lista[index];
+        if (element<min) {
+            min = element
+        }
+    }
+    return min
 }
 
