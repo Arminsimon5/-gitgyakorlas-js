f754486b017fb345fb34308b8d928f9fc69b6224
Simon Ármin <arminsimon5@gmail.com>
2023-02-27 11:06:48 +0100

    1)a feladat

---
+++ javascript.js
@@ -0,0 +1,11 @@
+function nagyobb(egesz1,egesz2) {
+    if (egesz1>egesz2) {
+        return egesz1
+    }
+    else if (egesz1<egesz2) {
+        return egesz2
+    }
+    else {
+        return "A két szám egyenlő."
+    }
+}
